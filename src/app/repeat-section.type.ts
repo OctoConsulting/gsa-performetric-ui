518f0a1d763820147f3b01216385a09c70db26e5
MeenuRaj <meenuraj979@gmail.com>
2020-07-23 15:44:32 -0400

    adding optional fields

--- src/app/repeat-section.type.ts
+++ src/app/repeat-section.type.ts
@@ -4,7 +4,8 @@ import { FieldArrayType } from '@ngx-formly/core';
 @Component({
   selector: 'formly-repeat-section',
   template: `
-    <div *ngFor="let fg of field.fieldGroup; let i = index;" class="row" style = "border: 1px solid;border-color: grey;padding: 1em; margin-top :0.5em;">
+    <div *ngFor="let fg of field.fieldGroup; let i = index;" class="row"
+    style = "border: 1px solid;border-color: lightgrey;padding: 1em; margin-top :0.5em;">
       <formly-field class="col" [field]="fg"></formly-field>
       <div class="col-sm-2 d-flex align-items-center" style = "margin-top: 1.5em">
         <button *ngIf="!(field.name == 'exec' && i==0)"
